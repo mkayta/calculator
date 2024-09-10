e4c59fc57f29583637e5c516ca6024de6625968b
Eng-Mohamed <mkayta2023@gmail.com>
2024-09-10 19:30:03 +0300

    I done this calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,23 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculator(num1, num2, operator) {
+    let calculate;
+
+    if (operator === "+") {
+        calculate = num1 + num2;
+    } else if (operator === "-") {
+        calculate = num1 - num2;
+    } else if (operator === "*") {
+        calculate = num1 * num2;
+    } else if (operator === "/") {
+            calculate = num1 / num2;
+    } else {
+        console.log("Error: Invalid operator.");
+        return;
+    }
+
+
+    console.log(calculate);
+}
+
+calculator(50,5,"/")
